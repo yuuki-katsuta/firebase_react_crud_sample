8eccc1f301d786628f0c97624837dd09e3fb1b76
yuuki-katsuta <reptpr0314@gmail.com>
2021-01-17 20:06:50 +0900

    firestore連携

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,13 +7,32 @@ import Button from '@material-ui/core/Button';
 
 const Home = () => {
   const { currentUser } = useContext(AuthContext);
+  const [spots, setSpot] = useState([]);
 
   const db = firebase.firestore();
+
+  //データ取得
+  useEffect(() => {
+    console.log('発火');
+    const spotsRef = db.collection('spots').where('uid', '==', currentUser.uid);
+    spotsRef.get().then((snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.data());
+      });
+    });
+  }, [db, currentUser]);
 
   return (
     <div>
       <h1>ようこそ {currentUser.displayName} !!</h1>
-      <h3>{currentUser.displayName}のやること</h3>
+      <h1>{currentUser.uid}</h1>
+      <div>
+        <h2>Spots</h2>
+        {spots.map((spot, idx) => (
+          <div key={idx}>{spot}</div>
+        ))}
+      </div>
+
       <Button
         style={{ margin: '20px 0' }}
         color='primary'
